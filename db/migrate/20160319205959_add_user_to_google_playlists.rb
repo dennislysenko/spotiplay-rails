994166ed47c3f7bc0ff933cb166394c090e8b3f3
Dennis Lysenko <dennis.s.lysenko@gmail.com>
2016-03-21 04:38:44 -0400

    rework two migrations that violate postgresql constraints

--- db/migrate/20160319205959_add_user_to_google_playlists.rb
+++ db/migrate/20160319205959_add_user_to_google_playlists.rb
@@ -1,6 +1,5 @@
 class AddUserToGooglePlaylists < ActiveRecord::Migration
   def change
     add_reference :google_playlists, :user, index: true, foreign_key: true
-    remove_reference :google_playlists, :user_id, index: true, foreign_key: true
   end
 end
